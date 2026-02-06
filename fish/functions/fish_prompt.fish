97e57dae4d6be23235a3c38c42a94717b9c122bf
simon <155461549+tr1rd@users.noreply.github.com>
2026-02-07 00:24:31 +0100

    Create fish_prompt.fish

---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,24 @@
+function fish_prompt
+        if not set -q VIRTUAL_ENV_DISABLE_PROMPT
+                set -g VIRTUAL_ENV_DISABLE_PROMPT true
+        end
+        printf '%s' $USER
+        printf ' at '
+    
+        set_color magenta
+        echo -n (prompt_hostname)
+        set_color normal
+        printf ' in '
+    
+        set_color $fish_color_cwd
+        printf '%s' (prompt_pwd)
+        set_color normal
+    
+        # Line 2
+        echo
+        if test -n "$VIRTUAL_ENV"
+                printf "(%s) " (set_color purple)(path basename $VIRTUAL_ENV)(set_color normal)
+        end
+        printf '↪ '
+        set_color normal
+end
